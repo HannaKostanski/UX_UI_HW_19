2e021f7f7b5024eedfca412c10fb991164ad04fd
HannaKostanski <hkostanski@gmail.com>
2024-02-11 17:23:21 -0500

    Final Commit
    Fixed js file - hopefully this will be the last commit

--- js/index.js
+++ js/index.js
@@ -2,4 +2,26 @@
  
  console.log("your index is just fine");
 
- 
+ $(document).ready(function(){
+    $(".box, .box2, .box3").hover(
+      function() {
+        $(this).css("transform", "scale(1.1)"); 
+      },
+      function() {
+        $(this).css("transform", "scale(1)"); 
+      }
+    );
+
+    $(".box2, .box3").click(function(){
+      var panelId = $(this).data('panel'); 
+      var $panel = $("#panel" + panelId); 
+      
+      $panel.slideToggle(600);
+    });
+    $(".box").click(function(){
+      var panelId = $(this).data('panel'); 
+      var $panel = $("#panel"); 
+      
+      $panel.slideToggle(600);
+    });
+  });
